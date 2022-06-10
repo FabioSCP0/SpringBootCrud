1223384e725ec585e35deac3f5a439c21b2c7d8d
Fabio <fabio.s0@hotmail.com>
2022-06-10 11:07:10 -0300

    Adiciona as funcionalidades da CONSULTA PAGINADA

--- ProjetoSB/src/main/java/com/github/FabioSCP0/ProjetoSB/controllers/ProdutoController.java
+++ ProjetoSB/src/main/java/com/github/FabioSCP0/ProjetoSB/controllers/ProdutoController.java
@@ -1,10 +1,14 @@
 package com.github.FabioSCP0.ProjetoSB.controllers;
 
+import java.util.Optional;
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +34,17 @@ public class ProdutoController {
 	@GetMapping
 	public Iterable<Produto> obterProdutos() {
 		return produtoRepository.findAll();
+	}
+
+	@GetMapping(path = "/pagina/{numeroPagina}/{qtdePagina}")
+	public Iterable<Produto> obterProdutosPorPagina(@PathVariable int numeroPagina, @PathVariable int qtdePagina) {
+
+		if(numeroPagina < 0) numeroPagina = 0;
+		if(qtdePagina <= 0) qtdePagina = 1;
+		if(qtdePagina>=5) qtdePagina=5;
+
+		PageRequest page = PageRequest.of(numeroPagina, qtdePagina);
+		return produtoRepository.findAll(page);
 	}
 
 	@GetMapping(path="/{id}")
--- ProjetoSB/src/main/java/com/github/FabioSCP0/ProjetoSB/models/repositories/ProdutoRepository.java
+++ ProjetoSB/src/main/java/com/github/FabioSCP0/ProjetoSB/models/repositories/ProdutoRepository.java
@@ -1,9 +1,6 @@
 package com.github.FabioSCP0.ProjetoSB.models.repositories;
 
-import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.data.repository.PagingAndSortingRepository;
 import com.github.FabioSCP0.ProjetoSB.models.entity.Produto;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer>{
-
-}
+public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer>{}
