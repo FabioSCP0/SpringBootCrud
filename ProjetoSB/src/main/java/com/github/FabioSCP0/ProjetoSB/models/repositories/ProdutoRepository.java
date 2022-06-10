1c1832e0a8eae6e11746168cbd4629a4df76482d
Fabio <fabio.s0@hotmail.com>
2022-06-10 11:17:03 -0300

    Adiciona as funcionalidades da CONSULTA POR NOME

--- ProjetoSB/src/main/java/com/github/FabioSCP0/ProjetoSB/models/repositories/ProdutoRepository.java
+++ ProjetoSB/src/main/java/com/github/FabioSCP0/ProjetoSB/models/repositories/ProdutoRepository.java
@@ -3,4 +3,8 @@ package com.github.FabioSCP0.ProjetoSB.models.repositories;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import com.github.FabioSCP0.ProjetoSB.models.entity.Produto;
 
-public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer>{}
+public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer>{
+
+    public Iterable<Produto> findByNomeContainingIgnoreCase(String parteNome);
+
+}
